afc8b694f014f6528a2f2ffd73687dc764f99af7
51x <six@keemail.me>
2017-01-12 19:44:57 +0100

    note

--- chaos_test/knowledge/0_parse.sh
+++ chaos_test/knowledge/0_parse.sh
@@ -1,2 +1,7 @@
 # first idea
 head -1 * | sed "s/~conncets~//g"
+
+# maybe python
+python -c "import difflib ; f1='t' ; f2=f1; difflib.SequenceMatcher(None, f1, f2)"
+
+# or restructure to .connect files?
